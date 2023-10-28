dc1ee638c75904d705fd942e441176f5ff1784ca
webcodeacademy <webcodeacademyinfo@gmail.com>
2023-10-28 18:34:49 +0400

    mariamIordanishvili

---
+++ js/chatbox.js
@@ -0,0 +1,6 @@
+function showchatbox(){
+        document.getElementById("chatbox").style.display="block";
+}
+function hidechatbox(){
+    document.getElementById("chatbox").style.display="none";
+}
